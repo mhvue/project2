3d219c223ca2086d30620cb1a0f71e993f73331e
hallieraew <hallieweintraub@gmail.com>
2020-03-16 20:48:43 -0500

    models complete for three tables

--- models/login.js
+++ models/login.js
@@ -1,10 +1,52 @@
-module.exports = function(sequelize, DataTypes) {
-  var Login = sequelize.define("Login", {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    name: DataTypes.STRING,
-    position: DataTypes.STRING
-  });
-  return Login;
-};
+module.exports = function (sequelize, DataTypes) {
+  var User = sequelize.define("user", {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
 
+    firstname: {
+      type: DataTypes.STRING,
+      notEmpty: true
+    },
+
+    lastname: {
+      type: DataTypes.STRING,
+      notEmpty: true
+    },
+
+    username: {
+      type: DataTypes.TEXT
+    },
+
+    about: {
+      type: DataTypes.TEXT
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    last_login: {
+      type: DataTypes.DATE
+    },
+
+    status: {
+      type: DataTypes.ENUM('active', 'inactive'),
+      defaultValue: 'active'
+    }
+
+  });
+
+  return User;
+
+}
--- models/pto.js
+++ models/pto.js
@@ -1,9 +1,37 @@
-module.exports = function(sequelize, DataTypes) {
-    var PTO = sequelize.define("PTO", {
-      name: DataTypes.STRING,
-      day: DataTypes.STRING,
-      startTime: DataTypes.INTEGER,
-      endTime: DataTypes.INTEGER
+module.exports = function (sequelize, DataTypes) {
+    var PTO = sequelize.define("pto", {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+          },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            msg: 'Please enter your first name'
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            msg: 'Please enter your last name'
+        },
+        date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        startTime: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        endTime: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        approved: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false
+        }
     });
     return PTO;
-  };
+};
