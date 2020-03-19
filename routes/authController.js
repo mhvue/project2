3d09d151658a6033b11506355e8e8c849f5589ca
Mai Houa Vue <mhvue4@gmail.com>
2020-03-18 21:21:12 -0500

    added starting code for sign up page with auth

---
+++ routes/authController.js
@@ -0,0 +1,10 @@
+ //starting code for authController 
+
+module.exports = function (){
+
+    //the below should take us to sign up if it works
+exports.signup = function(req, res) {
+    // res.render("signup"); 
+    console.log("hellloo")
+}
+}
