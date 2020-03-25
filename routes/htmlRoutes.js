3561ddb79791d0797e97fde1ea93761cf97bfe0e
hallieraew <hallieweintraub@gmail.com>
2020-03-25 15:55:05 -0500

    styling on form is good

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,40 +8,40 @@ module.exports = function(app) {
   });
 
   app.get("/signup", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
    });
 
    app.get("/signin", function(req, res) { //was index here
-    res.sendFile(path.join(__dirname, "../views/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 
    });
 
   //LOGIN  PAGE W/ HOME OR INDEX 
   app.get("/loggedin", function(req, res) { //was index here
-   res.sendFile(path.join(__dirname, "../views/loggedIn.html"));
+   res.sendFile(path.join(__dirname, "../public/loggedIn.html"));
   
   });
 
   // SCHEDULE PAGE = display the schedule page of current schedule 
   app.get("/schedule", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/schedulePage.html"));
+    res.sendFile(path.join(__dirname, "../public/schedulePage.html"));
   });
 
 
   //availability
   app.get("/avail", function(req,res){ 
-    res.sendFile(path.join(__dirname, "../views/avail.html")); 
+    res.sendFile(path.join(__dirname, "../public/avail.html")); 
   });
 
 
   //REQUEST OFF page  and //model shows the request was sent instead of seperate page 
   app.get("/requestoff", function(req,res){ 
-    res.sendFile(path.join(__dirname, "../views/requestOff.html")); 
+    res.sendFile(path.join(__dirname, "../public/requestOff.html")); 
   });
 
    //user LOGS OUT Page = this is the page that shows when user logs out
   app.get("/logout", function(req,res) {
-    // res.sendFile(path.join(__dirname, "../views/logOut.html")); 
+    // res.sendFile(path.join(__dirname, "../public/logOut.html")); 
     // OR go back to home page 
     res.redirect("/signin");
 
