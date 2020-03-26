8561427681708db9afed844efc5732fed0b33c8c
Mai Houa Vue <mhvue4@gmail.com>
2020-03-25 19:26:38 -0500

    added and deleted code not needed.

--- config/passport/passport-logic.js
+++ config/passport/passport-logic.js
@@ -5,51 +5,51 @@ var LocalStrategy = require("passport-local").Strategy;
 
 module.exports = function (passport, user) {
     //SIGN-UP 
-    //   passport.use("local-signup", new LocalStrategy(
-    //         {
-    //             usernameField: "email",
-    //             passwordField: "password",
-    //             passReqToCallback: true
-    //         }, 
-    //         function(req, email, password, done){
-    //             var generateHash = function(password) {
-    //                 return bCrypt.hashSync(password, 8 , null);
-    //             };
-    //             console.log("meeeeee");
+      passport.use("local-signup", new LocalStrategy(
+            {
+                usernameField: "email",
+                passwordField: "password",
+                passReqToCallback: true
+            }, 
+            function(req, email, password, done){
+                var generateHash = function(password) {
+                    return bCrypt.hashSync(password, 8 , null);
+                };
+                console.log("meeeeee");
 
-    //             UserDB.user.findOne({
-    //                 where: {
-    //                     email: email
-    //                 }
-    //             }).then(function(user) {
-    //                 if (user) {
-    //                     return done(null, false, {
-    //                         message: "That email is already taken"
-    //                     });
-    //                 } else {
-    //                     var userPassword = generateHash(password);
-    //                     var data =
-    //                     {
-    //                         email: req.body.email,
-    //                         password: userPassword,
-    //                         firstname: req.body.firstname,
-    //                         lastname: req.body.lastname
-    //                     };
+                UserDB.user.findOne({
+                    where: {
+                        email: email
+                    }
+                }).then(function(user) {
+                    if (user) {
+                        return done(null, false, {
+                            message: "That email is already taken"
+                        });
+                    } else {
+                        var userPassword = generateHash(password);
+                        var data =
+                        {
+                            email: req.body.email,
+                            password: userPassword,
+                            firstname: req.body.firstname,
+                            lastname: req.body.lastname
+                        };
 
-    //                     UserDB.user.create(data).then(function(newUser){
-    //                         console.log(newUser);
-    //                         if(!newUser){
-    //                             return done(null, false);
-    //                         }
-    //                         if (newUser){
-    //                             return done(null, newUser);
-    //                         }
-    //                     });
-    //                 }
-    //                 console.log("created: " + data);
-    //             });
-    //         }
-    //     ));
+                        UserDB.user.create(data).then(function(newUser){
+                            console.log(newUser);
+                            if(!newUser){
+                                return done(null, false);
+                            }
+                            if (newUser){
+                                return done(null, newUser);
+                            }
+                        });
+                    }
+                    console.log("created: " + data);
+                });
+            }
+        ));
 
     // SIGN-IN for existing users
     passport.use("signin", new LocalStrategy(
