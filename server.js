ad11d56fcc3ed0d670d80a4f6bf5ad4b43b20f35
hallieraew <hallieweintraub@gmail.com>
2020-03-27 18:03:10 -0500

    changing back the server sync to false

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ require("./routes/htmlRoutes")(app);
 
 
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 if (process.env.NODE_ENV === "test") {
     syncOptions.force = true;
